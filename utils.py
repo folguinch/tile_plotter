106b802a1535a9f555d77385a053c032f4dff8d2
Fernando <f.olguin.ch@gmail.com>
2022-06-10 16:22:21 +0800

    Convert to python 3, improve docs

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
+"""Plotting utilities."""
 from typing import Union, Optional, Callable, Tuple
 
+from astropy.coordinates import SkyCoord
 import astropy.stats as apystats
 import astropy.units as u
 import numpy as np
@@ -96,11 +98,12 @@ def auto_vmin(data: u.Quantity,
       velocity_fraction: optional; fraction of the minimum value of the data to
         use if data_type=velocity.
       map_type: optional; type of map.
+      log: optional; logging function.
     """
     if map_type.lower() in ['intensity', 'pvmap']:
         if rms is None:
-            rms = quick_rms(data)
-            log(f'Quick rms = {rms:.3e}')
+            rms = apystats.mad_std(data, ignore_nan=True)
+            log(f'MAD std = {rms:.3e}')
         else:
             log(f'Input rms = {rms:.3e}')
         vmin = nrms * rms
@@ -130,12 +133,6 @@ def auto_vmax(data: u.Quantity,
         vmax = fraction * maximum
     else:
         vmax = np.nanmax(data) * fraction
-    #if dtype in ['intensity', 'pvmap']:
-    #    vmax = np.nanmax(data)*frac
-    #elif dtype=='velocity':
-    #    vmax = np.nanmax(data)*vfrac
-    #else:
-    #    raise KeyError('dtype %s not recognized' % dtype)
     return to_sigfig(vmax.to(data.unit))
 
 def auto_vminmax(data: u.Quantity, 
@@ -167,23 +164,38 @@ def get_nlevels(max_val: u.Quantity, min_val: u.Quantity, stretch: str,
 
     return nlevels
 
-def auto_log_levels(data: Optional[u.Quantity] = None, 
-                    rms: Optional[u.Quantity] = None, 
-                    max_val: Optional[u.Quantity] = None,
-                    min_val: Optional[u.Quantity] = None,
-                    nsigma: float = 5., 
-                    base: float = 2., 
-                    nlevels: Optional[int] = None,
-                    min_nlevels: int = 1, 
-                    negative_nsigma: Optional[float] = None,
-                    min_base: float = 1.1,
-                    log: Callable = print) -> u.Quantity:
-    """
+def auto_levels(data: Optional[u.Quantity] = None,
+                rms: Optional[u.Quantity] = None,
+                max_val: Optional[u.Quantity] = None,
+                min_val: Optional[u.Quantity] = None,
+                nsigma: float = 5.,
+                base: float = 2.,
+                nlevels: Optional[int] = None,
+                min_nlevels: int = 1,
+                negative_nsigma: Optional[float] = None,
+                min_base: float = 1.1,
+                nsigmalevel: Optional = None,
+                log: Callable = print) -> u.Quantity:
+    """Determine the levels for contour plots.
+
+    Args:
+      data: optional; data to extract the levels from.
+      rms: optional; rms of the data.
+      max_val: optional; maximum contour.
+      min_val: optional; minumum contour.
+      nsigma: optional; minimum contour over rms value.
+      negative_nsigma: optional; maximum contour for negative levels.
+      base: optional; logarithmic base.
+      nlevels: optional; number of levels.
+      min_nlevels: optional; minimum number of levels.
+      min_base: optional; minimum base for refining.
+      nsigmalevel: optional; only one level at this nsigma value.
+      log: optional; logging function.
     """
     # Value checks
     log('Calculating levels')
     if min_base > base:
-        log('Minimum base < base, setting min_base = base')
+        log('Minimum base > base, setting min_base = base')
         min_base = base
     if nlevels and nlevels < min_nlevels:
         log('Minimum nlevels > nlevels, setting nlevels = min_nlevels')
@@ -195,10 +207,10 @@ def auto_log_levels(data: Optional[u.Quantity] = None,
     log('Determining levels from data:')
     if rms is None and data is not None:
         log('Using automatic rms for levels')
-        rms = quick_rms(data)
+        rms = apystats.mad_std(data, ignore_nan=True)
     elif ((rms is None and data is None) or
           (rms is not None and nlevels is None)):
-        raise Exception('Could not determine levels')
+        raise ValueError('Could not determine levels')
     rms = to_sigfig(rms)
     log(f'Getting levels from rms = {rms}')
 
@@ -241,31 +253,144 @@ def auto_log_levels(data: Optional[u.Quantity] = None,
     aux_levels = [nsigma * base**i for i in range(nlevels)]
 
     # Negative contours
-    if negative_nsigma:
+    if negative_nsigma is not None:
+        aux = -negative_nsigma * rms
         if min_val is None and data is not None:
             min_val = np.nanmin(data)
+            log(f'Minimum data: {min_val}')
+            log(f'Start negative level (-{negative_nsigma}sigma): {aux}')
             loop = True
         elif min_val is None and data is None:
             log('Could not determine minimum value')
             log('Skipping negative levels')
             loop = False
         else:
+            log(f'Minimum data: {min_val}')
+            log(f'Start negative level (-{negative_nsigma}sigma): {aux}')
             loop = True
 
         # Get negative levels
         i = -1
         while loop:
-            newlevel = i * abs(negative_nsigma) * rms
-            if newlevel <= np.nanmin(data):
+            factor = i * abs(negative_nsigma)
+            newlevel = factor * rms
+            if newlevel <= min_val:
                 break
-            elif -factor not in aux:
-                aux = [-factor] + aux
+            elif factor not in aux_levels:
+                aux_levels = [factor] + aux_levels
             i = i * base
 
     # Levels    
-    levels = np.array(aux) * rms
-    log(f'Levels/rms = {aux}')
+    levels = np.array(aux_levels) * rms
+    log(f'Levels/rms = {aux_levels}')
     log(f'Levels = {levels}')
 
     return levels
 
+def generate_label(name: str, unit: Optional[u.Unit] = None,
+                   unit_fmt: str = '({})') -> str:
+    """Generate a standard label from the input values.
+
+    Args:
+      name: name in the label.
+      unit: optional; unit in the label.
+      unit_fmt: optional; format of the unit string.
+    """
+    label = [f'{name.title()}']
+    if unit is not None:
+        label.append(unit_fmt.format(unit))
+    return ' '.join(label)
+
+def get_artist_positions(values: str, artist: str,
+                         separator: str = ',') -> tuple:
+    """Extract the position values for the input `artist`.
+
+    It uses `artist` to determine the type of the output.
+
+    Args:
+      values: string where the positions are obtained.
+      artist: artist type.
+      separator: optional; separator between values.
+    """
+    vals = values.split(',')
+    positions = []
+    for val in vals:
+        if artist in ['scatters', 'arcs']:
+            try:
+                ra, dec, frame = val.split()
+            except ValueError:
+                ra, dec = val.split()
+                frame = 'icrs'
+            positions.append(SkyCoord(ra, dec, frame=frame))
+        else:
+            positions.append(tuple(map(float, val.split())))
+
+    return positions
+
+def get_artist_properties(artist: str, config: 'ConfigParserAdv',
+                          separator: str = ',' ) -> dict:
+    """Extract the properties of the artists from `config`.
+
+    This function creates a dictionary that contain the markers positions as
+    keys and a dictionary of the artist properties as its value. The keywords
+    are extracted from the option name in the `config`.
+
+    Example:
+    A configuration file with the following options:
+
+    ```init
+    scatter = pos1x pos1y, pos2x pos2y
+    scatter_color = r, w
+    ```
+
+    will be converted to:
+
+    ```python
+    properties = {'positions': ((pos1x, pos1y), (pos2x, pos2y)),
+                  'properties': ({'color': 'r'}, {'color': 'w'})}
+    ```
+    
+    Each dictionary can be used as keyword input for the corresponding
+    `matplotlib` function.
+
+    Args:
+      artist: name of the artist.
+      config: configuration parser proxy.
+      separator: optional; separator between values of an config option.
+    """
+    # Length
+    positions = get_artist_positions(config[artist], artist,
+                                     separator=separator)
+    nprops = len(positions)
+
+    # Iterate over properties
+    props = ()
+    for opt in config:
+        # Filter
+        if not opt.startswith(artist) or opt == artist:
+            continue
+        
+        # Iterate over positions
+        prop = opt.split('_')[-1]
+        for i in range(nprops):
+            if prop in ['size', 'width', 'height', 'angle', 's', 'alpha']:
+                val = config.getvalue(opt, n=i, dtype=float, allow_global=True,
+                                      sep=separator)
+            elif prop in ['zorder']:
+                val = config.getvalue(opt, n=i, dtype=int, allow_global=True,
+                                      sep=separator)
+            else:
+                val = config.getvalue(opt, n=i, allow_global=True,
+                                      sep=separator)
+
+            # Special value
+            if val == 'none' and prop != 'fillstyle':
+                val = None
+
+            # Fill the properties tuple
+            try:
+                props[i].update({prop: val})
+            except IndexError:
+                props += ({prop: val},)
+
+    return {'positions': positions, 'properties': props}
