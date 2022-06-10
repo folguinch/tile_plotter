96c04594afad7f9729867048bad07416106f8e41
Fernando <f.olguin.ch@gmail.com>
2022-06-10 16:23:06 +0800

    Implement plot handling and data loading

---
+++ data_loaders.py
@@ -0,0 +1,59 @@
+"""Implement loaders for different types of data."""
+from typing import Callable, Tuple
+
+from astropy.io import fits
+from astropy import wcs
+
+# Loader functions
+def load_image(filename: 'Path') -> Tuple[fits.PrimaryHDU, wcs.WCS]:
+    """Load FITS file."""
+    image = fits.open(filename)[0]
+    proj = wcs.WCS(image.header, naxis=['longitude', 'latitude'])
+
+    return image, proj
+
+# Available loaders
+LOADERS = {
+    'image': load_image,
+    'contour': load_image,
+    'pvmap': load_image,
+    'moment': load_image,
+}
+
+# General purpose loader
+def data_loader(config: 'ConfigParserAdv',
+                log: Callable = print) -> Tuple['Data', 'Projection']:
+    """Find a loader and loads the data from a config parser proxy.
+
+    If the `loader` option is given in the configuration, then this is used to
+    determine the loader. The value of the `loader` option specifies option
+    where the data file name is stored. Otherwise, it will iterate over the
+    loaders available and use the one matching the options.
+
+    Args:
+      config: config parser proxy.
+      log: optional; logging function.
+
+    Returns:
+      The loaded data.
+      The projection of the data.
+      The data type.
+    """
+    if 'loader' in config:
+        log('Using loader option')
+        key = config['loader']
+        filename = config.getpath(key)
+        loader = LOADER[key]
+    else:
+        for key, loader in LOADERS.items():
+            if key in config:
+                log(f'Data option found: {key}')
+                filename = config.getpath(key)
+                break
+        else:
+            raise KeyError('Cannot find loader for data')
+
+    log(f'Loading data: {filename}')
+    data, proj = loader(filename)
+
+    return data, proj, key
---
+++ handlers.py
@@ -0,0 +1,28 @@
+"""Manage the available plot handlers."""
+from map_plotter import MapHandler
+
+HANDLERS = {
+    'image': MapHandler,
+    'contour': MapHandler,
+}
+
+def get_handler(config: 'ConfigParser') -> 'Handler':
+    """Find a plotting handler based on the `config` proxy options.
+
+    If a `handler` option is given in the configuration, then this is used to
+    obtain the respective handler. Otherwise, it will iterate over the stored
+    handlers and find a key matching the options.
+
+    Args:
+      config: a config parser proxy.
+    """
+    # Check for specific option
+    if 'handler' in config:
+        return HANDLERS[config['handler']]
+
+    # Iterate over keys
+    for key, val in HANDLERS.items():
+        if key in config:
+            return val
+
+    raise KeyError('Could not find a suitable plot handler')
