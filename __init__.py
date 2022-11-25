0a0369a74d0b2faf1f9f4ba4d701ad14b2f0259a
Fernando Olguin <f.olguin.ch@gmail.com>
2022-11-25 13:58:30 +0800

    Register additional color maps

--- __init__.py
+++ __init__.py
@@ -1,3 +1,13 @@
 """
 .. include:: README.md
 """
+try:
+    from cmcrameri import cm
+    import matplotlib as mpl
+
+    print(('Registering color maps: '
+           f'{cm.broc.name} {cm.vik.name}'))
+    mpl.colormaps.register(cm.broc)
+    mpl.colormaps.register(cm.vik)
+except ImportError:
+    pass
