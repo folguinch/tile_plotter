4a6c953ce15400c07badb17e37dbeb146a4b88c7
Fernando Olguin <f.olguin.ch@gmail.com>
2022-12-05 09:09:00 +0800

    Register more colors

--- __init__.py
+++ __init__.py
@@ -6,8 +6,22 @@ try:
     import matplotlib as mpl
 
     print(('Registering color maps: '
-           f'{cm.broc.name} {cm.vik.name}'))
+           f'{cm.broc.name}'
+           f'{cm.vik.name}'
+           f'{cm.imola.name}'
+           f'{cm.oslo_r.name}'
+           f'{cm.bamako.name}'
+           f'{cm.nuuk.name}'
+           f'{cm.tokyo.name}'
+           f'{cm.turku.name}'
+           ))
     mpl.colormaps.register(cm.broc)
     mpl.colormaps.register(cm.vik)
+    mpl.colormaps.register(cm.imola)
+    mpl.colormaps.register(cm.bamako)
+    mpl.colormaps.register(cm.nuuk)
+    mpl.colormaps.register(cm.oslo_r)
+    mpl.colormaps.register(cm.tokyo)
+    mpl.colormaps.register(cm.turku)
 except ImportError:
     pass
